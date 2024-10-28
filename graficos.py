a44f544c9cd25997a39bff8790febfd3222de7f6
51672 <eduardo.martins2@tjce.jus.br>
2024-10-28 16:04:47 -0300

    locus graph

--- graficos.py
+++ graficos.py
@@ -8,14 +8,12 @@ data_path = './csvs'
 num_usuarios = [10, 100, 1000]
 spawn_rates = [1.0, 2.0, 3.0]
 
-# Mapeamento dos tipos de requisição para rótulos mais amigáveis
 tipos_requisicao = {
     "/2024/10/28/post-com-texto-de-400-kb/": "post com texto de 400kb",
     "/wp-content/uploads/2024/10/1920x1080-Hd-Pictures-Download-1024x576.jpg": "post com imagem de 300kb",
     "/wp-content/uploads/2024/10/626311.jpg": "post com imagem de 1mb"
 }
 
-# Inicializa um dicionário para armazenar os tempos de resposta para cada tipo
 tempo_resposta_por_tipo = {
     tipo: {f"{n_usuarios} usuários": [] for n_usuarios in num_usuarios} 
     for tipo in tipos_requisicao.keys()
@@ -29,7 +27,6 @@ for n_usuarios in num_usuarios:
         if os.path.isfile(file_path):
             data = pd.read_csv(file_path)
             
-            # Calcula a média do tempo de resposta para cada tipo de requisição
             for tipo in tipos_requisicao.keys():
                 tipo_data = data[data["Name"] == tipo]
                 if not tipo_data.empty:
@@ -42,7 +39,6 @@ for n_usuarios in num_usuarios:
             for tipo in tipos_requisicao.keys():
                 tempo_resposta_por_tipo[tipo][f"{n_usuarios} usuários"].append(None)
 
-# Gera um gráfico para cada tipo de requisição com o novo rótulo
 for tipo, label in tipos_requisicao.items():
     tempos_por_usuarios = tempo_resposta_por_tipo[tipo]
     fig, ax = plt.subplots(figsize=(10, 6))
@@ -60,6 +56,5 @@ for tipo, label in tipos_requisicao.items():
     ax.legend(title="Número de Usuários")
     ax.grid(True, axis='y', linestyle='--', alpha=0.7)
 
-    # Salva o gráfico com o novo nome de arquivo
     plt.savefig(f"grafico_{label.replace(' ', '_')}.png")
-    plt.close(fig)  # Fecha o gráfico para evitar sobrecarga
+    plt.close(fig) 

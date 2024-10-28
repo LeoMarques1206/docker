d612c4e02b4fec4496a31a74fb50591e5363415a
51672 <eduardo.martins2@tjce.jus.br>
2024-10-28 12:05:53 -0300

    locust finished

--- locust-scripts/locustfile.py
+++ locust-scripts/locustfile.py
@@ -1,18 +1,20 @@
 from locust import HttpUser, TaskSet, task, between
+import os
 
 class UserBehavior(TaskSet):
     @task(2)
     def view_post_image_1mb(self):
-        self.client.get("/post-with-1mb-image")  # Rota para post com imagem de 1 MB
+        self.client.get("/wp-content/uploads/2024/10/626311.jpg")  
 
     @task(2)
     def view_post_text_400kb(self):
-        self.client.get("/post-with-400kb-text")  # Rota para post com texto de 400 KB
+        self.client.get("/2024/10/28/post-com-texto-de-400-kb/")  
 
     @task(1)
     def view_post_image_300kb(self):
-        self.client.get("/post-with-300kb-image")  # Rota para post com imagem de 300 KB
+        self.client.get("/wp-content/uploads/2024/10/1920x1080-Hd-Pictures-Download-1024x576.jpg")  
 
 class WebsiteUser(HttpUser):
     tasks = [UserBehavior]
-    wait_time = between(1, 3)  # Tempo de espera entre as requisições
+    wait_time = between(1, 3)
+    host = os.getenv("ATTACKED_HOST") 
